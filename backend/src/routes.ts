1c759863768138726e1b7cb0068fb16b9ddafc70
yassine20011 <medalgamer47@gmaim.com>
2024-12-06 00:55:12 +0100

    Add route to display registered API routes using express-list-routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,14 @@
 import { Router } from 'express';
 import authController from './auth/auth.controller';
+import router from './auth/auth.controller';
+import expressListRoutes from 'express-list-routes';
 
-const api = Router().use(authController);
+const api = Router();
+
+api.get('/', (req, res) => {
+  res.send(expressListRoutes(api, { prefix: '/v1' }));
+});
+
+api.use(authController);
 
 export default Router().use('/v1', api);
