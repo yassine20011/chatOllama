50b9c6b836ff65aacc970c4a04c8c21aaedfd059
zakaria1x <zaka80ouahdani@gmail.com>
2024-12-12 15:09:34 +0100

    adding the sign up page

---
+++ frontend/src/pages/signup.tsx
@@ -0,0 +1,128 @@
+import { Button } from '../components/ui/button';
+import { Input } from "@/components/ui/input";
+import OllamaSvg from '@/assets/ollama.svg';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const Signup = () => {
+
+  const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+    confirmPassword: z.string().min(8, "Password must be at least 8 characters long"),
+  }).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    // TODO: Handle form submission
+    console.log(values);
+  };
+
+  return (
+    <section className="bg-gray-50">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <div className="flex items-center justify-center">
+              <a
+                href="/"
+                className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
+              >
+                <img
+                  className="w-8 h-8 mr-2"
+                  src={OllamaSvg}
+                  alt="ollama"
+                />
+                ChatOllama <span className="font-circle">●</span>
+              </a>
+            </div>
+            <Form {...form}>
+              <form className="space-y-4 md:space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="email@example.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input type="password" placeholder="********" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Confirm Password</FormLabel>
+                      <FormControl>
+                        <Input type="password" placeholder="********" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button
+                  type="submit"
+                  className="w-full"
+                >
+                  Sign Up
+                </Button>
+                <p className="text-sm font-light text-gray-900">
+                  Already have an account?{" "}
+                  <a
+                    href="/signin"
+                    className="font-medium text-primary-600 hover:underline"
+                  >
+                    Sign in
+                  </a>
+                </p>
+              </form>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Signup;
