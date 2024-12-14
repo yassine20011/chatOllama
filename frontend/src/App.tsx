30f7f3dde740e1bc4678273afcb0e950ca50e636
yassine20011 <medalgamer47@gmaim.com>
2024-12-14 17:00:45 +0100

    Add authentication context and private route for conversation page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,24 @@ import { Routes, Route } from "react-router-dom";
 import Index from "./pages/index";
 import NotFound from "./pages/notFound";
 import Signin from "./pages/signin";
+import Conversation from './pages/conversation';
+import { AuthContextProvider } from './context/AuthContext';
+import PrivateRoute from './pages/priveteRoute';
 
 function App() {
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path='/chat' element={<PrivateRoute />}>
+            <Route path=':conversationId' element={<Conversation />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   )
 }
