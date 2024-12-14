f4a7d44559844ff60d3e157b96ae8e0b2c560465
yassine20011 <medalgamer47@gmaim.com>
2024-12-14 17:03:58 +0100

    Add AuthContext for user authentication management with login, signup, and logout functionality

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,128 @@
+import axiosInstance from '@/conf/axois-instance';
+import { createContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from "jwt-decode";
+
+
+interface loginInput {
+    email: string;
+    password: string;
+}
+
+interface signupInput {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
+
+
+interface AuthContextType {
+    isAuth: boolean;
+    token: string | null;
+    login: (input: loginInput) => Promise<void>;
+    signup: (input: signupInput) => Promise<void>;
+    logout: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextType>({
+    isAuth: false,
+    token: null,
+    login: async () => { },
+    signup: async () => { },
+    logout: async () => { },
+});
+
+
+const checkToken = (token: string | null) => {
+    
+    if (!token) {
+        return false;
+    }    
+    const decodedToken = jwtDecode(token);
+    if (decodedToken.exp && decodedToken.exp * 1000 < Date.now()) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+function AuthContextProvider({ children }: any) {
+    const [user, setUser] = useState(null);
+    const [isAuth, setIsAuth] = useState(checkToken(localStorage.getItem('token')));
+    const [token, setToken] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    const navigate = useNavigate();
+    const login = async (input: loginInput) => {
+        // Call login endpoint
+        try {
+            const response = await axiosInstance.post('/v1/signin', input)
+            // If successful, setUser(user)
+            if (response.status === 200) {
+                console.log(response.data);
+                const { token, ...user } = response.data;
+                setUser(user);
+                setIsAuth(true);
+                setToken(token);
+                localStorage.setItem('token', token);
+                navigate('/chat/c');
+                console.log("isAuth", isAuth);
+            }
+            else {
+                setUser(null);
+            }
+        } catch (error) {
+            setUser(null);
+            throw error;
+        }
+    }
+
+    const signup = async (input: signupInput) => {
+        // Call signup endpoint
+        // If successful, setUser(user)
+        // Else, setUser(null)
+    }
+
+    const logout = async () => {
+        setUser(null);
+        setIsAuth(false);
+        localStorage.removeItem('token');
+        navigate('/');
+    }
+
+    useEffect(() => {
+        // Check if user is logged in
+        const token = localStorage.getItem('token');
+        if (token) {
+            const decodedToken = jwtDecode(token);
+
+            if (decodedToken.exp && decodedToken.exp * 1000 < Date.now()) {
+                logout();
+            } else {
+                setIsAuth(true);
+            }
+        } else {
+            setIsAuth(false);
+            setUser(null);
+        }
+
+        setLoading(false);
+    }, []);
+
+
+
+    const values = {
+        isAuth, setIsAuth ,token, login, signup, logout
+    }
+
+    return (
+        <AuthContext.Provider value={values}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export { AuthContext, AuthContextProvider };
