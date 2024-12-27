0c858dd0dd4973b16c6a4520ea7756a2409ab209
yassine20011 <medalgamer47@gmaim.com>
2024-12-27 14:33:49 +0100

    Add message creation and retrieval endpoints to message controller

--- backend/src/message/message.controller.ts
+++ backend/src/message/message.controller.ts
@@ -1,3 +1,49 @@
 import express, { NextFunction, Request, Response, Router } from "express";
 import auth from "@/auth/auth";
 import { jwtDecode } from "jwt-decode";
+import { createMessage, getMessages } from "./message.service";
+
+const router = Router();
+router.use(express.json());
+
+router.post(
+  "/create/message",
+  auth.required as any,
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const data = req.body;
+
+      if (!data.conversationId || !data.userInput || !data.modelResponse) {
+        res
+          .status(400)
+          .json({
+            message: "ConversationId, userInput and modelResponse are required",
+          });
+        return;
+      }
+      const message = await createMessage(
+        data.conversationId,
+        data.userInput,
+        data.modelResponse
+      );
+      res.status(201).json(message);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get(
+  "/get/messages/:conversationId",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const conversationId = req.params.conversationId;
+      const messages = await getMessages(conversationId);
+      res.status(200).json(messages);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+export default router;
